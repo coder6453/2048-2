ed830a4a95b9cfdf3991f484f083e75314869779
coder6453 <91762474+coder6453@users.noreply.github.com>
2024-09-14 11:09:56 -0500

    Update grid.js

--- js/grid.js
+++ js/grid.js
@@ -7,10 +7,10 @@ function Grid(size, previousState) {
 Grid.prototype.empty = function () {
   var cells = [];
 
-  for (var x = 0; x < this.size-2; x++) {
+  for (var x = 0; x < this.size; x++) {
     var row = cells[x] = [];
 
-    for (var y = 0; y < this.size-2; y++) {
+    for (var y = 0; y < this.size; y++) {
       row.push(null);
     }
   }
